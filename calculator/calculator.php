ccefddf15774a4b8121eb35903e7a25a97ece0db
Pieter Oliver <po12180@my.bristol.ac.uk>
2016-12-15 15:36:39 +0000

    Made the calculator do the basics.

--- calculator/calculator.php
+++ calculator/calculator.php
@@ -4,15 +4,37 @@
     $line = fgets($handle);
     if(trim($line) == "+") {
         echo("Addition. Provide integers\n");
-        
+        $int_1 = fgets($handle);
+        echo("+\n");
+        $int_2 = fgets($handle);
+        $added = $int_1 + $int_2;
+        echo('=' . $added."\n");
     }
-    if(trim($line) == "-"){
-        
+    elseif (trim($line) == "-") {
+        echo("Subtraction. Provide integers\n");
+        $int_1 = fgets($handle);
+        echo("-\n");
+        $int_2 = fgets($handle);
+        $subtraction = $int_1 - $int_2;
+        echo('=' . $subtraction."\n");
     }
-    if(trim($line) == "*"){
-        
+    elseif (trim($line) == "*") {
+        echo("Multiplication. Provide integers\n");
+        $int_1 = fgets($handle);
+        echo("*\n");
+        $int_2 = fgets($handle);
+        $multipled = $int_1 * $int_2;
+        echo('=' . $multipled ."\n");
     }
-    if(trim($line) == "//"){
-        
+    elseif (trim($line) == "/"){
+        echo("Division. Provide integers\n");
+        $int_1 = fgets($handle);
+        echo("/\n");
+        $int_2 = fgets($handle);
+        $multipled = $int_1 / $int_2;
+        echo('=' . $multipled ."\n");
+    }
+    else{
+        echo("Ed Voice: What, are you doing!\n");
     }
 ?>
